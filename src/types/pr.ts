01282164edda0f95b229d2a9cfd7cdce7ccf4cd8
dsher23 <DAVIDSHERWOOD15@HOTMAIL.COM>
2025-04-24 20:25:23 +0100

    Update pr.ts

--- src/types/pr.ts
+++ src/types/pr.ts
@@ -1,18 +1,6 @@
-
-export type WeightUnit = 'kg' | 'lbs' | 'stone';
-export type MeasurementUnit = 'cm' | 'in';
-
-export interface PR {
-  exerciseId: string;
+export interface PRLift {
+  id: string;
+  exercise: string;
   weight: number;
   date: string;
-  reps: number;
-  workoutId?: string;
-  isDirectEntry?: boolean;
-}
-
-export interface UnitSystem {
-  bodyWeightUnit: WeightUnit;
-  bodyMeasurementUnit: MeasurementUnit;
-  liftingWeightUnit: WeightUnit;
 }
