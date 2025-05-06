52e1565313c8370737660989356e0a9b44175903
dsher23 <DAVIDSHERWOOD15@HOTMAIL.COM>
2025-05-06 12:05:47 +0100

    Create window.d.ts

---
+++ src/types/window.d.ts
@@ -0,0 +1,10 @@
+// /src/types/window.d.ts
+import { RecaptchaVerifier } from "firebase/auth";
+
+declare global {
+  interface Window {
+    recaptchaVerifier: RecaptchaVerifier | undefined;
+  }
+}
+
+export {};
