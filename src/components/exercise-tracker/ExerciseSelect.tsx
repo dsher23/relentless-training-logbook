efcf95d5c93701c65c110297f33efa8ce8f2d3e2
dsher23 <DAVIDSHERWOOD15@HOTMAIL.COM>
2025-05-07 11:31:42 +0100

    Delete src directory

--- src/components/exercise-tracker/ExerciseSelect.tsx
+++
@@ -1,5 +0,0 @@
-
-// This file re-exports our main ExerciseSelect component
-import { ExerciseSelect } from "@/components/ExerciseSelect";
-export { ExerciseSelect };
-export default ExerciseSelect;
--- src/hooks/useUser.ts
+++
@@ -1,8 +0,0 @@
-
-import { useContext } from "react";
-import { AppContext } from "@/context/AppContext";
-
-export const useUser = () => {
-  const { user } = useContext(AppContext);
-  return user;
-};
